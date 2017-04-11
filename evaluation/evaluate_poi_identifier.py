86b0c310208a40f23cdb777a2ed85ebd68879536
WillieMaddox <willie.maddox@gmail.com>
2017-04-11 14:02:19 -0500

    PEP8

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -16,16 +16,15 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-### add more features to features_list!
+# add more features to features_list!
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
-
-### your code goes here 
+# your code goes here
 
 
